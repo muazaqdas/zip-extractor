e9895ed68239daeac50ec6693fe93bc6f243be37
Muaz Aqdas <muazaqdasssh@gmail.com>
2022-07-19 21:55:44 +0530

    heading & footer added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import Footer from './Components/footer';
+import Heading from './Components/heading';
 
 function App() {
   return (
     <div className="App">
+    <Heading/>
+    <Footer/>
     </div>
   );
 }
---
+++ src/Components/heading.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "../assets/style/heading.css"
+
+
+const Heading = () => {
+  return (
+    <>
+        <header>
+          Zip Extractor is a small and easy online tool that can extract
+          over 70 types of compressed files, such as 7z, zipx, rar, tar, exe,
+          dmg and much more.
+        </header>
+    </>
+  )
+}
+
+export default Heading
